aca898884b796714be7e81fdd5266dcb8e85d15b
andrewtlu <andrewlulu2012@gmail.com>
2025-09-20 16:06:22 -0400

    Add TodoItem schema & helper setup script

---
+++ src/app/Todo.tsx
@@ -0,0 +1,16 @@
+export type TodoItem = {
+    text: string;
+    completed: boolean;
+}
+
+export const Todo = ({
+  text,
+  completed,
+}: TodoItem) => {
+  return (
+    // TODO: finish me!
+    <div className="flex flex-row gap-[32px]">
+      <div>{text}</div>
+    </div>
+  );
+};
