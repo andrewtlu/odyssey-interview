c99b528b5cda1225b64edc76b96b1c43cc9cc2d4
Robert Jarman <robertpj235@gmail.com>
2025-09-23 23:17:49 -0400

    Task 2 Update 1.0

--- src/app/Todo/Todo.tsx
+++ src/app/Todo/Todo.tsx
@@ -8,8 +8,18 @@ export type TodoItem = {
 export const Todo = ({ id, text, completed }: TodoItem) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return (
-    <div className="flex flex-row gap-[32px]">
-      <div>{text}</div>
+    <div className="flex flex-row gap-[32px] p-3 mb-2 rounded-lg border ${
+      completed
+      ? 'bg-gray-100 opacity-60 border-gray-300'
+      : 'bg-white border-gray-200'
+      }">
+      <input
+        type="checkbox"
+        checked={completed}
+        readOnly
+        className="mt-1"
+      />
+      <div className={completed ? 'line-through text-gray-500' : ''}>{text}</div>
     </div>
   );
 };
