e3c9e40c951a90fa071ad3d19d8636cf83ad207f
Caleb Jennings <calebbirtch.j@gmail.com>
2025-09-25 09:48:14 -0400

    feat: toggle checkbox/opacity on click and update TodoItem styling

--- src/app/Todo/Todo.tsx
+++ src/app/Todo/Todo.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 export type TodoItem = {
   id: number;
@@ -7,8 +8,18 @@ export type TodoItem = {
 };
 
 export const Todo = ({ id, text, completed }: TodoItem) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setClicked(event.target.checked);
+  };
+
   return (
-    <div className="flex flex-row gap-[32px]">
+    <div
+      onClick={() => setClicked(!clicked)}
+      className={`flex flex-row gap-[4px] bg-blue-500 rounded-2xl p-2 m-1 hover:scale-[1.02] transition-transform  ${clicked ? "opacity-70" : "opacity-100"}`}
+    >
+      <input type="checkbox" checked={clicked}/>
       <div>{text}</div>
     </div>
   );
