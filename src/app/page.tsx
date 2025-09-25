7f0c1b7a62037cba147e650a49bdd78264a05a8b
mlb20058 <mlb20058@gmail.com>
2025-09-25 19:31:23 -0400

    rest of tasks (database handling)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import {
   IconBrandPrisma,
   IconBrandTailwind,
 } from "@tabler/icons-react";
+import { fetchAllTodos } from "./Database/actions";
 import { TodoList } from "./Todo/TodoList";
 
 /**
@@ -17,10 +18,11 @@ import { TodoList } from "./Todo/TodoList";
  * @returns the home page at `localhost:3000/`
  */
 const Home = async () => {
+  const todos = await fetchAllTodos();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-xl">
-        <TodoList />
+        <TodoList initialTodos={todos}/>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
