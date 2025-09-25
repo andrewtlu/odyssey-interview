4629b01f49061002e1cf78eb97a91faeedc29555
Caleb Jennings <calebbirtch.j@gmail.com>
2025-09-25 16:24:00 -0400

    feat: clear input field after new value entered

--- src/app/Todo/TodoList.tsx
+++ src/app/Todo/TodoList.tsx
@@ -27,6 +27,7 @@ export const TodoList = ({ initialTodos }: { initialTodos?: TodoItem[] }) => {
           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto hover:cursor-pointer"
           onClick={() => {
             if (newTodoText.trim() === "") return;
+            setNewTodoText("");
             setTodos([
               ...todos,
               { id: todos.length + 1, text: newTodoText, completed: false },
