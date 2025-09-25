127d016177538be40369c4f76b169f1e14e160f0
Caleb Jennings <calebbirtch.j@gmail.com>
2025-09-25 10:00:01 -0400

    fix: remove unused handleChange

--- src/app/Todo/Todo.tsx
+++ src/app/Todo/Todo.tsx
@@ -10,16 +10,12 @@ export type TodoItem = {
 export const Todo = ({ id, text, completed }: TodoItem) => {
   const [clicked, setClicked] = useState(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setClicked(event.target.checked);
-  };
-
   return (
     <div
       onClick={() => setClicked(!clicked)}
       className={`flex flex-row gap-[4px] bg-blue-500 rounded-2xl p-2 m-1 hover:scale-[1.02] transition-transform  ${clicked ? "opacity-70" : "opacity-100"}`}
     >
-      <input type="checkbox" checked={clicked}/>
+      <input type="checkbox" checked={clicked} />
       <div>{text}</div>
     </div>
   );
