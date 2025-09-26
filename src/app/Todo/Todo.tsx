a6f876a66934b34a76f030bf772114bbd127cbd4
Caleb Jennings <calebbirtch.j@gmail.com>
2025-09-25 20:23:51 -0400

    feat: add onToggle and onDelete handlers (not working yet)

--- src/app/Todo/Todo.tsx
+++ src/app/Todo/Todo.tsx
@@ -1,33 +1,40 @@
 "use client";
+import { on } from "events";
 import { useState } from "react";
 
 export type TodoItem = {
   id: number;
   text: string;
   completed: boolean;
+  onToggle: (id: number) => void;
+  onDelete: (id: number) => void;
 };
 
-export const Todo = ({ id, text, completed }: TodoItem) => {
+export const Todo = ({ id, text, completed, onToggle, onDelete }: TodoItem) => {
   const [clicked, setClicked] = useState(false);
 
   return (
     <div
-  onClick={() => setClicked(!clicked)}
-  className={`flex flex-row items-center gap-3 
+      onClick={() => {
+        setClicked(!clicked);
+        onToggle(id);
+      }}
+      className={`flex flex-row items-center gap-3 
     rounded-2xl px-4 py-3 m-2 cursor-pointer 
     bg-gradient-to-r from-blue-500 to-teal-600
     hover:scale-[1.01] active:scale-[0.99] 
     transition-all duration-300 ease-out
     ${clicked ? "opacity-70 ring-2 ring-teal-400" : "opacity-100"}
   `}
->
-  <input
-    type="checkbox"
-    checked={clicked}
-    readOnly
-    className="w-4 h-4 accent-teal-500 cursor-pointer"
-  />
-  <div className="text-white font-medium tracking-wide">{text}</div>
-</div>
+    >
+      <input
+        type="checkbox"
+        checked={clicked}
+        readOnly
+        className="w-4 h-4 accent-teal-500 cursor-pointer"
+      />
+      <div className="text-white font-medium tracking-wide">{text}</div>
+      <button onClick={() => onDelete(id)}>X</button>
+    </div>
   );
 };
--- src/app/Todo/TodoList.tsx
+++ src/app/Todo/TodoList.tsx
@@ -8,12 +8,20 @@ export const TodoList = ({ initialTodos }: { initialTodos?: TodoItem[] }) => {
   const [todos, setTodos] = useState<TodoItem[]>(initialTodos || []);
   const [newTodoText, setNewTodoText] = useState("");
 
+  function handleToggle(id: number) {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo));
+  }
+
+  function handleDelete(id: number) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
   return (
     <>
       <div className="font-medium text-2xl">My Todo List</div>
       <ul className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left bg-content w-full px-2 py-1 rounded-md flex flex-col">
         {todos.map((val) => (
-          <Todo key={val.id} {...val} />
+          <Todo key={val.id} {...val} onToggle={handleToggle} onDelete={handleDelete}/>
         ))}
       </ul>
       <div className="flex gap-4 items-center flex-col sm:flex-row w-full">
