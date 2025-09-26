9fe39af612672ef13db0830f3b2cea691aedb0a6
shaunika1 <shaunika.shaunika@gmail.com>
2025-09-26 13:29:01 -0400

    work on task 3

--- src/app/Todo/TodoList.tsx
+++ src/app/Todo/TodoList.tsx
@@ -5,7 +5,7 @@ import { Todo, TodoItem } from "./Todo";
 import { useState } from "react";
 
 export const TodoList = ({ initialTodos }: { initialTodos?: TodoItem[] }) => {
-  const todos: TodoItem[] = [
+  const [todos, setTodos]= useState<TodoItem[]> ([
     {
       id: 1,
       text: "item 1",
@@ -21,12 +21,24 @@ export const TodoList = ({ initialTodos }: { initialTodos?: TodoItem[] }) => {
       text: "item 3",
       completed: true,
     },
-  ];
+  ]);
   const [newTodoText, setNewTodoText] = useState("");
+
+  //adding new todo function below
+  const newTodo: TodoItem={
+    id: Date.now(), //not sure about this Date.now() function
+    text: newTodoText,
+    completed:false,
+  }
+
+  setTodos([...todos, newTodo]);
+  setNewTodoText("");
+};
+
 
   return (
     <>
-      <div className="font-medium text-2xl text-align: center">My Todo List</div>
+      <div className="font-medium text-2xl text-center">My Todo List</div>
       
       <ul className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left bg-content w-full px-2 py-1 rounded-md flex flex-col">
         {todos.map((val) => (
@@ -46,7 +58,8 @@ export const TodoList = ({ initialTodos }: { initialTodos?: TodoItem[] }) => {
           value={newTodoText}
           onChange={(e) => setNewTodoText(e.target.value)}
         />
-        <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto hover:cursor-pointer">
+        <button onClick={handleAddTodo}
+        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto hover:cursor-pointer">
           <IconPlus />
           Add Item
         </button>
