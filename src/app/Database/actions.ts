b85e16fddda44dc22bed994b90487266d24521e5
Caleb Jennings <calebbirtch.j@gmail.com>
2025-09-26 22:06:39 -0400

    feat: implement prisma functions for todos

--- src/app/Database/actions.ts
+++ src/app/Database/actions.ts
@@ -6,14 +6,26 @@ export const fetchAllTodos = async () => {
   return await prisma.todoItem.findMany();
 };
 
-export const createTodo = async () => {
-  return null;
+export const createTodo = async (t: string) => {
+  return await prisma.todoItem.create({
+    data: {
+      text: t,
+      completed: false,
+    },
+  });
 };
 
-export const completeTodo = async () => {
-  return null;
+export const completeTodo = async (id: number) => {
+  return await prisma.todoItem.update({
+    where: { id },
+    data: { completed: true },
+  });
 };
 
-export const deleteTodo = async () => {
-  return null;
+export const deleteTodo = async (id: number) => {
+  return await prisma.todoItem.delete({
+    where: {
+      id: id,
+    },
+  });
 };
