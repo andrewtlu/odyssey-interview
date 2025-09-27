7afbc048d79b750c6df61f189a7049ec0e763612
shaunika1 <shaunika.shaunika@gmail.com>
2025-09-27 17:39:58 -0400

    finish oa

--- src/app/Todo/Todo.tsx
+++ src/app/Todo/Todo.tsx
@@ -10,7 +10,7 @@ export const Todo = ({ id, text, completed, onDelete, onComplete }: TodoItem & {
   return (
     <div className="flex flex-row gap-[32px] rounded-lg bg-blue-100 border-2 border-black text-black">
       <input type="checkbox" checked={completed}  onChange={onComplete}/>
-      <div className="flex-1">{text}</div>
+      <div className={`flex-1 ${completed ? "line-through text-gray-500" :""}`} >{text}</div>
       <button onClick={onDelete} className ="border-2 border-black text-red-500 bg-gray-100 rounded-lg hover:bg-black"> Delete</button>
     </div>
   );
